0e452f1d88f94a3852d1d56d272e6872c61280bb
Johan S. Wind <johansokrates@hotmail.com>
2024-12-16 14:59:04 +0100

    Add triton attn for big head sizes

--- wind_rwkv/rwkv7/__init__.py
+++ wind_rwkv/rwkv7/__init__.py
@@ -3,6 +3,7 @@ from .rwkv7_expand_loras import expand_loras
 from .rwkv7_attn_ln import attn_ln, load_attn_ln
 from .rwkv7_attn import attn, load_attn
 from .rwkv7_attn_triton import attn_triton
+from .rwkv7_attn_triton_bighead import attn_triton_bighead
 from .time_mixer import TimeMixer
 
 __version__ = '0.1'
